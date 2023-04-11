c06a32a7d8d97403852b9152e167d272033d102c
Ghosteye <yqcyecong@163.com>
2023-04-11 23:56:22 +0800

    feat: Add mini-zx

---
+++ mini-zx/examples/hello.mjs
@@ -0,0 +1,11 @@
+#!/usr/bin/env zx
+// import { $ } from 'zx'
+
+// console.log("Hello, World!");
+
+// // 运行 cat 命令
+// const content = await $`cat package.json`
+// console.log(content)
+
+
+$`cat package.json`
